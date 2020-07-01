274578e6c1a72270db8b195da4c4f71138294ba1
thaina <thaina.carvalho@bluefoot.com.br>
2020-07-01 13:43:38 -0300

    Cria aplicacao em react

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 'use strict'
+var React = require('react')
 
-function sum(val1, val2) {
-  return val1 + val2;
-}
 
-module.exports = sum
+var Title = React.createClass({
+  render: function() {
+    return React.createElement('h1', null, 'Titulo')
+  }
+})
+
+module.exports = Title
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict'
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
 
-var sum = require('./app')
-
-console.log(sum(1,2));
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
 
